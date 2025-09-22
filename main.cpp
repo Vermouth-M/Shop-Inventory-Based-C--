9b656d7637c81d84f81ac145500935dc7132f030
Vermouth-M <decadedxtsu@gmail.com>
2025-09-22 21:06:50 +0700

    asd

--- main.cpp
+++ main.cpp
@@ -4,54 +4,120 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+struct Book
+{
+    string kode;
+    string nama;
+    int stok;
+    int harga;
+};
 
-    vector<string> books(n);
-    for (int i = 0; i < n; i++) {
-        cin >> books[i];
+void bubblesort(vector<Book> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j].kode > arr[j + 1].kode)
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
     }
+}
 
+int main()
+{
     int q;
     cin >> q;
+    Book b;
 
-    while (q--) {
+    vector<Book> books;
+
+    while (q--)
+    {
         string operation;
         cin >> operation;
 
-        if (operation == "CARI") {
-            string code;
-            cin >> code;
-
-            bool found = false;
-            for (int i = 0; i < n; i++) {
-                if (books[i] == code) {
-                    found = true;
+        if (operation == "TAMBAH")
+        {
+            cin >> b.kode >> b.nama >> b.stok >> b.harga;
+            bool exists = false;
+            for (auto &bk : books)
+            {
+                if (bk.kode == b.kode)
+                {
+                    exists = true;
                     break;
                 }
             }
 
-            cout << (found ? "ADA" : "TIDAK ADA") << endl;
+            if (exists)
+            {
+                cout << "KODE SUDAH DIMASUKAN" << endl;
+            }
+            else
+            {
+                books.push_back(b);
+            }
+        }
+        else if (operation == "CARI")
+        {
+            string code;
+            cin >> code;
 
-        } else if (operation == "HITUNG") {
-            string substring;
-            cin >> substring;
-
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (books[i].find(substring) != string::npos) {
-                    count++;
+            bool found = false;
+            for (auto &b : books)
+            {
+                if (b.kode == code)
+                {
+                    found = true;
+                    break;
                 }
             }
-            cout << count << endl;
+            cout << (found ? "ADA" : "TIDAK ADA") << endl;
+        }
+        else if (operation == "JUAL")
+        {
+            string code;
+            int jmljual;
+            cin >> code;
+            cin >> jmljual;
 
-        } else if (operation == "URUT") {
-            vector<string> sorted_books = books;
-            sort(sorted_books.begin(), sorted_books.end());
-
-            for (const string &book : sorted_books) {
-                cout << book << endl;
+            bool found = false;
+            for (auto &b : books)
+            {
+                if (b.kode == code)
+                {
+                    b.stok -= jmljual;
+                    cout << ("Berhasil Terjual") << endl;
+                    break;
+                }
+                else
+                {
+                    cout << ("BARANG TIDAK DITEMUKAN") << endl;
+                    break;
+                }
+                if (b.kode == code)
+                {
+                    b.stok -= jmljual;
+                    cout << ("Berhasil Terjual") << endl;
+                    break;
+                }
+                else if (jmljual > b.stok)
+                {
+                    cout << ("Berlebih hasil -") << endl;
+                }
+            }
+        }
+        else if (operation == "LAPORAN")
+        {
+            bubblesort(books);
+            for (auto &b : books)
+            {
+                cout << b.kode << " " << b.nama << " "
+                     << b.stok << " " << b.harga << endl;
             }
         }
     }
